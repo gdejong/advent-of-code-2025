bf54ff7e5f8f0b33321229b167ed56314c5c22e4
Gijs de Jong <gijs_d.j@hotmail.com>
2025-12-01 22:50:25 +0100

    Solve day 01 part 2

--- cmd/day01/main.go
+++ cmd/day01/main.go
@@ -12,6 +12,8 @@ func main() {
 	in := input.Content("cmd/day01/real_input.txt")
 
 	fmt.Println(part1(in))
+
+	fmt.Println(part2(in))
 }
 
 func part1(lines []string) int {
@@ -39,4 +41,57 @@ func part1(lines []string) int {
 	}
 
 	return resetsToZero
+}
+
+func part2(lines []string) int {
+	dial := 50 // The dial starts by pointing at 50
+	pointedAtZero := 0
+
+	restAtZero := false
+
+	for _, line := range lines {
+		direction := line[0:1] // Either "L" or "R"
+		rotation, err := strconv.Atoi(line[1:])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// All full circle rotates count as one. Count those here and reduce the rest of the problem to a rotation between 1 and 99
+		if rotation > 100 {
+			pointedAtZero += rotation / 100 // for example R640 counts 6 resets...
+			rotation %= 100                 // and leaves 40 for the rest of the rotation.
+		}
+
+		if direction == "L" {
+			dial -= rotation
+
+			if dial <= 0 {
+				if !restAtZero {
+					pointedAtZero++
+				}
+				// If we landed on zero exactly don't +100
+				if dial < 0 {
+					dial += 100
+				}
+			}
+
+		} else if direction == "R" {
+			dial += rotation
+
+			if dial >= 100 {
+				if !restAtZero {
+					pointedAtZero++
+				}
+				dial -= 100
+			}
+
+		} else {
+			panic("invalid direction")
+		}
+
+		// mark if we ended exactly on zero, this helps us later to avoid counting resets twice
+		restAtZero = dial == 0
+	}
+
+	return pointedAtZero
 }
--- cmd/day01/main_test.go
+++ cmd/day01/main_test.go
@@ -7,11 +7,41 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	lines := input.Content("test_input.txt")
+	testData := input.Content("test_input.txt")
 
-	answer := part1(lines)
+	testAnswer := part1(testData)
 
-	if answer != 3 {
-		t.Errorf("wrong answer, got: %d", answer)
+	if testAnswer != 3 {
+		t.Errorf("wrong test answer, got: %d", testAnswer)
+	}
+
+	// and let's also assert the real input
+
+	realData := input.Content("real_input.txt")
+
+	realAnswer := part1(realData)
+
+	if realAnswer != 1018 {
+		t.Errorf("wrong real answer, got: %d", realAnswer)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	testData := input.Content("test_input.txt")
+
+	testAnswer := part2(testData)
+
+	if testAnswer != 6 {
+		t.Errorf("wrong test answer, got: %d", testAnswer)
+	}
+
+	// and let's also assert the real input
+
+	realData := input.Content("real_input.txt")
+
+	realAnswer := part2(realData)
+
+	if realAnswer != 5815 {
+		t.Errorf("wrong real answer, got: %d", realAnswer)
 	}
 }
