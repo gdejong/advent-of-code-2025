9837ff1bf390984e11216ba57080c082428ab912
Gijs de Jong <gijs_d.j@hotmail.com>
2025-12-04 20:13:41 +0100

    Added day00 template

---
+++ cmd/day00-template/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gdejong/advent-of-code-2025/internal/input"
+)
+
+func main() {
+	in := input.Content("cmd/day00-template/real_input.txt")
+
+	fmt.Println(part1(in))
+
+	fmt.Println(part2(in))
+}
+
+func part1(lines []string) int {
+	return 0
+}
+
+func part2(lines []string) int {
+	return 0
+}
